bc561c140dc17c8ba0842ccfaa880137393917ac
Luis Avila <luis@Luiss-MacBook-Pro.local>
2020-04-10 21:44:57 -0400

    Styled Components in React Native

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+// import { Text } from "react-native";
+import styled from "styled-components/native";
+import Card from "./Components/Cards";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up ok App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <TitleBar>
+          <Avatar source={require("./assets/avatar.jpg")} />
+          <Title>Welcome back,</Title>
+          <Name>Meng</Name>
+        </TitleBar>
+        <Subtitle>Continue Learning</Subtitle>
+        <Card />
+      </Container>
+    );
+  }
 }
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+`;
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//styc
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+const TitleBar = styled.View`
+  /* border: red; */
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
